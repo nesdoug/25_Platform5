6a87919e7d54869a3f961a44d1e0a2c5a7d89e9e
nesdoug <dougfraker@gmail.com>
2018-09-04 17:07:38 -0400

    new

---
+++ platformer5.c
@@ -0,0 +1,713 @@
+/*	example code for cc65, for NES
+ *  Scrolling Right with metatile system
+ *	, basic platformer
+ *	, with coins and enemies
+ *	using neslib
+ *	Doug Fraker 2018
+ */	
+ 
+#include "LIB/neslib.h"
+#include "LIB/nesdoug.h"
+#include "Sprites.h" // holds our metasprite data
+#include "platformer5.h"
+
+
+	
+	
+void main (void) {
+	
+	ppu_off(); // screen off
+	
+	song = SONG_GAME;
+	music_play(song);
+	
+	// load the palettes
+	pal_bg(palette_bg);
+	pal_spr(palette_sp);
+	
+	// use the second set of tiles for sprites
+	// both bg and sprites are set to 0 by default
+	bank_spr(1);
+	
+	set_vram_buffer(); // do at least once
+	clear_vram_buffer();
+	
+	load_room();
+	
+	ppu_on_all(); // turn on screen
+	
+	
+
+	
+	while (1){
+		// infinite loop
+		while(game_mode == MODE_GAME){
+			ppu_wait_nmi();
+			
+			set_music_speed(8);
+		
+			pad1 = pad_poll(0); // read the first controller
+			pad1_new = get_pad_new(0);
+			
+			clear_vram_buffer(); // do at the beginning of each frame
+			
+			// there is a visual delay of 1 frame, so properly you should
+			// 1. move user 2.check collisions 3.allow enemy moves 4.draw sprites
+			movement();
+			check_spr_objects(); // see which objects are on screen
+			sprite_collisions();
+			enemy_moves();
+			
+			// set scroll
+			set_scroll_x(scroll_x);
+			set_scroll_y(scroll_y);
+			draw_screen_R();
+			draw_sprites();
+			
+			if(pad1_new & PAD_START){
+				game_mode = MODE_PAUSE;
+				song = SONG_PAUSE;
+				music_play(song);
+				color_emphasis(COL_EMP_DARK);
+			}
+		}
+		while(game_mode == MODE_PAUSE){
+			ppu_wait_nmi();
+
+			clear_vram_buffer(); // reset every frame
+			pad1 = pad_poll(0); // read the first controller
+			pad1_new = get_pad_new(0);
+			
+			draw_sprites();
+			
+			if(pad1_new & PAD_START){
+				game_mode = MODE_GAME;
+				song = SONG_GAME;
+				music_play(song);
+				color_emphasis(COL_EMP_NORMAL);
+			}
+		}
+		
+	}
+}
+
+
+
+
+void load_room(void){
+	set_data_pointer(Rooms[0]);
+	set_mt_pointer(metatiles1); 
+	for(y=0; ;y+=0x20){
+		for(x=0; ;x+=0x20){
+			clear_vram_buffer(); // do each frame, and before putting anything in the buffer
+			address = get_ppu_addr(0, x, y);
+			index = (y & 0xf0) + (x >> 4);
+			buffer_4_mt(address, index); // ppu_address, index to the data
+			flush_vram_update_nmi();
+			if (x == 0xe0) break;
+		}
+		if (y == 0xe0) break;
+	}
+	
+	
+	
+	// a little bit in the next room
+	set_data_pointer(Rooms[1]);
+	for(y=0; ;y+=0x20){
+		x = 0;
+		clear_vram_buffer(); // do each frame, and before putting anything in the buffer
+		address = get_ppu_addr(1, x, y);
+		index = (y & 0xf0);
+		buffer_4_mt(address, index); // ppu_address, index to the data
+		flush_vram_update_nmi();
+		if (y == 0xe0) break;
+	}
+	clear_vram_buffer();
+	
+	// copy the room to the collision map
+	// the second one should auto-load with the scrolling code
+	memcpy (c_map, Rooms[0], 240);
+	
+	
+	sprite_obj_init();
+}
+
+
+
+
+void draw_sprites(void){
+	// clear all sprites from sprite buffer
+	oam_clear();
+
+	// reset index into the sprite buffer
+	sprid = 0;
+	
+	// draw 1 hero
+	if(direction == LEFT) {
+		sprid = oam_meta_spr(high_byte(BoxGuy1.x), high_byte(BoxGuy1.y), sprid, RoundSprL);
+	}
+	else{
+		sprid = oam_meta_spr(high_byte(BoxGuy1.x), high_byte(BoxGuy1.y), sprid, RoundSprR);
+	}
+	
+	
+	for(index = 0; index < MAX_COINS; ++index){
+		temp_y = coin_y[index];
+		if(temp_y == TURN_OFF) continue;
+		if(get_frame_count() & 8) ++temp_y; // bounce the coin
+		temp1 = coin_active[index];
+		temp2 = coin_x[index];
+		if(temp1 && (temp_y < 0xf0)) {
+			sprid = oam_meta_spr(temp2, temp_y, sprid, CoinSpr);
+		}
+	}
+	
+	
+	for(index = 0; index < MAX_ENEMY; ++index){
+		temp_y = enemy_y[index];
+		if(temp_y == TURN_OFF) continue;
+		temp1 = enemy_active[index];
+		temp2 = enemy_x[index];
+		if(temp2 > 0xf0) continue;
+		if(temp1 && (temp_y < 0xf0)) {
+			sprid = oam_meta_spr(temp2, temp_y, sprid, EnemySpr);
+		}
+	}
+	
+	
+	
+	// draw "coins" at the top in sprites
+	sprid = oam_meta_spr(16,16,sprid, CoinsSpr);
+	temp1 = coins + 0xf0;
+	sprid = oam_spr(64,16,temp1,3,sprid);
+}
+	
+
+	
+	
+void movement(void){
+	
+// handle x
+
+	old_x = BoxGuy1.x;
+	
+	if(pad1 & PAD_LEFT){
+		direction = LEFT;
+		if(BoxGuy1.x <= 0x100) {
+			BoxGuy1.vel_x = 0;
+			BoxGuy1.x = 0x100;
+		}
+		else if(BoxGuy1.x < 0x400) { // don't want to wrap around to the other side
+			BoxGuy1.vel_x = -0x100;
+		}
+		else {
+			BoxGuy1.vel_x -= ACCEL;
+			if(BoxGuy1.vel_x < -MAX_SPEED) BoxGuy1.vel_x = -MAX_SPEED;
+		}
+	}
+	else if (pad1 & PAD_RIGHT){
+		
+		direction = RIGHT;
+
+		BoxGuy1.vel_x += ACCEL;
+		if(BoxGuy1.vel_x > MAX_SPEED) BoxGuy1.vel_x = MAX_SPEED;
+	}
+	else { // nothing pressed
+		if(BoxGuy1.vel_x >= 0x100) BoxGuy1.vel_x -= ACCEL;
+		else if(BoxGuy1.vel_x < -0x100) BoxGuy1.vel_x += ACCEL;
+		else BoxGuy1.vel_x = 0;
+	}
+	
+	BoxGuy1.x += BoxGuy1.vel_x;
+	
+	if(BoxGuy1.x > 0xf800) { // make sure no wrap around to the other side
+		BoxGuy1.x = 0x100;
+		BoxGuy1.vel_x = 0;
+	} 
+	
+	L_R_switch = 1; // shinks the y values in bg_coll, less problems with head / feet collisions
+	
+	Generic.x = high_byte(BoxGuy1.x); // this is much faster than passing a pointer to BoxGuy1
+	Generic.y = high_byte(BoxGuy1.y);
+	Generic.width = HERO_WIDTH;
+	Generic.height = HERO_HEIGHT;
+	bg_collision();
+	if(collision_R && collision_L){ // if both true, probably half stuck in a wall
+		BoxGuy1.x = old_x;
+		BoxGuy1.vel_x = 0;
+	}
+	else if(collision_L) {
+		BoxGuy1.vel_x = 0;
+		high_byte(BoxGuy1.x) = high_byte(BoxGuy1.x) - eject_L;
+		
+	}
+	else if(collision_R) {
+		BoxGuy1.vel_x = 0;
+		high_byte(BoxGuy1.x) = high_byte(BoxGuy1.x) - eject_R;
+	} 
+
+
+	
+// handle y
+
+// gravity
+
+	// BoxGuy1.vel_y is signed
+	if(BoxGuy1.vel_y < 0x300){
+		BoxGuy1.vel_y += GRAVITY;
+	}
+	else{
+		BoxGuy1.vel_y = 0x300; // consistent
+	}
+	BoxGuy1.y += BoxGuy1.vel_y;
+	
+	L_R_switch = 0;
+	Generic.x = high_byte(BoxGuy1.x); // the rest should be the same
+	Generic.y = high_byte(BoxGuy1.y);
+	bg_collision();
+	
+	if(collision_U) {
+		high_byte(BoxGuy1.y) = high_byte(BoxGuy1.y) - eject_U;
+		BoxGuy1.vel_y = 0;
+	}
+	else if(collision_D) {
+		high_byte(BoxGuy1.y) = high_byte(BoxGuy1.y) - eject_D;
+		BoxGuy1.y &= 0xff00;
+		if(BoxGuy1.vel_y > 0) {
+			BoxGuy1.vel_y = 0;
+		}
+	}
+
+
+	// check collision down a little lower than hero
+	Generic.y = high_byte(BoxGuy1.y); // the rest should be the same
+	bg_check_low();
+	if(collision_D) {
+		if(pad1_new & PAD_A) {
+			BoxGuy1.vel_y = JUMP_VEL; // JUMP
+			sfx_play(SFX_JUMP, 0);
+		}
+		
+	}
+	
+	// do we need to load a new collision map? (scrolled into a new room)
+	if((scroll_x & 0xff) < 4){
+		new_cmap();
+	}
+	
+// scroll
+	temp5 = BoxGuy1.x;
+	if (BoxGuy1.x > MAX_RIGHT){
+		temp1 = (BoxGuy1.x - MAX_RIGHT) >> 8;
+		scroll_x += temp1;
+		high_byte(BoxGuy1.x) = high_byte(BoxGuy1.x) - temp1;
+	}
+
+	if(scroll_x >= MAX_SCROLL) {
+		scroll_x = MAX_SCROLL; // stop scrolling right, end of level
+		BoxGuy1.x = temp5; // but allow the x position to go all the way right
+		if(high_byte(BoxGuy1.x) >= 0xf1) {
+			BoxGuy1.x = 0xf100;
+		}
+	}
+}	
+
+
+
+
+void enemy_moves(void){
+	
+	temp1 = high_byte(BoxGuy1.x);
+	
+	if(get_frame_count() & 0x01) return; // half speed
+	
+	for(index = 0; index < MAX_ENEMY; ++index){
+		if(enemy_active[index]){
+			if(enemy_x[index] > temp1){
+				if(enemy_actual_x[index] == 0) --enemy_room[index];
+				--enemy_actual_x[index];
+			}
+			else if(enemy_x[index] < temp1){
+				++enemy_actual_x[index];
+				if(enemy_actual_x[index] == 0) ++enemy_room[index];
+			}
+		}
+	}
+}
+
+
+
+
+
+void bg_collision(void){
+	// note, uses bits in the metatile data to determine collision
+	// sprite collision with backgrounds
+	// load the object's x,y,width,height to Generic, then call this
+	
+
+	collision_L = 0;
+	collision_R = 0;
+	collision_U = 0;
+	collision_D = 0;
+	
+	if(Generic.y >= 0xf0) return;
+	
+	temp6 = temp5 = Generic.x + scroll_x; // upper left (temp6 = save for reuse)
+	temp1 = temp5 & 0xff; // low byte x
+	temp2 = temp5 >> 8; // high byte x
+	
+	eject_L = temp1 | 0xf0;
+	
+	temp3 = Generic.y; // y top
+	
+	eject_U = temp3 | 0xf0;
+	
+	if(L_R_switch) temp3 += 2; // fix bug, walking through walls
+	
+	bg_collision_sub();
+	
+	if(collision & COL_ALL){ // find a corner in the collision map
+		++collision_L;
+		++collision_U;
+	}
+	
+	// upper right
+	temp5 += Generic.width;
+	temp1 = temp5 & 0xff; // low byte x
+	temp2 = temp5 >> 8; // high byte x
+	
+	eject_R = (temp1 + 1) & 0x0f;
+	
+	// temp3 is unchanged
+	bg_collision_sub();
+	
+	if(collision & COL_ALL){ // find a corner in the collision map
+		++collision_R;
+		++collision_U;
+	}
+	
+	
+	// again, lower
+	
+	// bottom right, x hasn't changed
+	
+	temp3 = Generic.y + Generic.height; //y bottom
+	if(L_R_switch) temp3 -= 2; // fix bug, walking through walls
+	eject_D = (temp3 + 1) & 0x0f;
+	if(temp3 >= 0xf0) return;
+	
+	bg_collision_sub();
+	
+	if(collision & COL_ALL){ // find a corner in the collision map
+		++collision_R;
+	}
+	if(collision & (COL_DOWN|COL_ALL)){ // find a corner in the collision map
+		++collision_D;
+	}
+	
+	// bottom left
+	temp1 = temp6 & 0xff; // low byte x
+	temp2 = temp6 >> 8; // high byte x
+	
+	//temp3, y is unchanged
+
+	bg_collision_sub();
+	
+	if(collision & COL_ALL){ // find a corner in the collision map
+		++collision_L;
+	}
+	if(collision & (COL_DOWN|COL_ALL)){ // find a corner in the collision map
+		++collision_D;
+	}
+
+	if((temp3 & 0x0f) > 3) collision_D = 0; // for platforms, only collide with the top 3 pixels
+
+}
+
+
+
+void bg_collision_sub(void){
+	coordinates = (temp1 >> 4) + (temp3 & 0xf0);
+	
+	map = temp2&1; // high byte
+	if(!map){
+		collision = c_map[coordinates];
+	}
+	else{
+		collision = c_map2[coordinates];
+	}
+	
+	collision = is_solid[collision];
+}
+
+
+
+void draw_screen_R(void){
+	// scrolling to the right, draw metatiles as we go
+	pseudo_scroll_x = scroll_x + 0x120;
+	
+	temp1 = pseudo_scroll_x >> 8;
+	
+	set_data_pointer(Rooms[temp1]);
+	nt = temp1 & 1;
+	x = pseudo_scroll_x & 0xff;
+	
+	// important that the main loop clears the vram_buffer
+	
+	switch(scroll_count){
+		case 0:
+			address = get_ppu_addr(nt, x, 0);
+			index = 0 + (x >> 4);
+			buffer_4_mt(address, index); // ppu_address, index to the data
+			
+			address = get_ppu_addr(nt, x, 0x20);
+			index = 0x20 + (x >> 4);
+			buffer_4_mt(address, index); // ppu_address, index to the data
+			break;
+			
+		case 1:
+			address = get_ppu_addr(nt, x, 0x40);
+			index = 0x40 + (x >> 4);
+			buffer_4_mt(address, index); // ppu_address, index to the data
+			
+			address = get_ppu_addr(nt, x, 0x60);
+			index = 0x60 + (x >> 4);
+			buffer_4_mt(address, index); // ppu_address, index to the data
+			break;
+			
+		case 2:
+			address = get_ppu_addr(nt, x, 0x80);
+			index = 0x80 + (x >> 4);
+			buffer_4_mt(address, index); // ppu_address, index to the data
+			
+			address = get_ppu_addr(nt, x, 0xa0);
+			index = 0xa0 + (x >> 4);
+			buffer_4_mt(address, index); // ppu_address, index to the data
+			break;
+			
+		default:
+			address = get_ppu_addr(nt, x, 0xc0);
+			index = 0xc0 + (x >> 4);
+			buffer_4_mt(address, index); // ppu_address, index to the data
+			
+			address = get_ppu_addr(nt, x, 0xe0);
+			index = 0xe0 + (x >> 4);
+			buffer_4_mt(address, index); // ppu_address, index to the data
+	}
+
+
+	++scroll_count;
+	scroll_count &= 3; //mask off top bits, keep it 0-3
+}
+
+
+
+
+void new_cmap(void){
+	// copy a new collision map to one of the 2 c_map arrays
+	room = ((scroll_x >> 8) +1); //high byte = room, one to the right
+	
+	map = room & 1; //even or odd?
+	if(!map){
+		memcpy (c_map, Rooms[room], 240);
+	}
+	else{
+		memcpy (c_map2, Rooms[room], 240);
+	}
+}
+
+
+
+
+void bg_check_low(void){
+
+	// floor collisions
+	collision_D = 0;
+	
+	temp5 = Generic.x + scroll_x;    //left
+	temp1 = temp5 & 0xff; //low byte
+	temp2 = temp5 >> 8; //high byte
+	
+	temp3 = Generic.y + Generic.height + 1; // bottom
+	
+	if(temp3 >= 0xf0) return;
+	
+	bg_collision_sub();
+	
+	if(collision & (COL_DOWN|COL_ALL)){ // find a corner in the collision map
+		++collision_D;
+	}
+	
+	
+	//temp5 = right
+	temp5 += Generic.width;
+	temp1 = temp5 & 0xff; //low byte
+	temp2 = temp5 >> 8; //high byte
+	
+	//temp3 is unchanged
+	bg_collision_sub();
+	
+	if(collision & (COL_DOWN|COL_ALL)){ // find a corner in the collision map
+		++collision_D;
+	}
+	
+	if((temp3 & 0x0f) > 3) collision_D = 0; // for platforms, only collide with the top 3 pixels
+
+}
+
+
+
+
+void sprite_collisions(void){
+
+	Generic.x = high_byte(BoxGuy1.x);
+	Generic.y = high_byte(BoxGuy1.y);
+	Generic.width = HERO_WIDTH;
+	Generic.height = HERO_HEIGHT;
+	
+	Generic2.width = COIN_WIDTH;
+	Generic2.height = COIN_HEIGHT;
+	
+	for(index = 0; index < MAX_COINS; ++index){
+		if(coin_active[index]){
+			Generic2.x = coin_x[index];
+			Generic2.y = coin_y[index];
+			if(check_collision(&Generic, &Generic2)){
+				coin_y[index] = TURN_OFF;
+				sfx_play(SFX_DING, 0);
+				++coins;
+			}
+		}
+	}
+
+	Generic2.width = ENEMY_WIDTH;
+	Generic2.height = ENEMY_HEIGHT;
+	
+	for(index = 0; index < MAX_ENEMY; ++index){
+		if(enemy_active[index]){
+			Generic2.x = enemy_x[index];
+			Generic2.y = enemy_y[index];
+			if(check_collision(&Generic, &Generic2)){
+				enemy_y[index] = TURN_OFF;
+				sfx_play(SFX_NOISE, 0);
+				if(coins) --coins;
+			}
+		}
+	}
+}
+
+
+
+
+void check_spr_objects(void){
+	// mark each object "active" if they are, and get the screen x
+	
+	for(index = 0; index < MAX_COINS; ++index){
+		coin_active[index] = 0; //default to zero
+		if(coin_y[index] != TURN_OFF){
+			temp5 = (coin_room[index] << 8) + coin_actual_x[index];
+			coin_active[index] = get_position();
+			coin_x[index] = temp_x; // screen x coords
+		}
+
+	}
+	
+
+	for(index = 0; index < MAX_ENEMY; ++index){
+		enemy_active[index] = 0; //default to zero
+		if(enemy_y[index] != TURN_OFF){
+			temp5 = (enemy_room[index] << 8) + enemy_actual_x[index];
+			enemy_active[index] = get_position();
+			enemy_x[index] = temp_x; // screen x coords
+		}
+
+	}
+
+	
+}
+
+
+
+char get_position(void){
+	// is it in range ? return 1 if yes
+	
+	temp5 -= scroll_x;
+	temp_x = temp5 & 0xff;
+	if(high_byte(temp5)) return 0;
+	return 1;
+}
+
+
+
+// cc65 is very slow if 2 arrays/pointers are on the same line, so I
+// broke them into 2 separate lines with temp1 as a passing variable
+void sprite_obj_init(void){
+
+	pointer = level_1_coins;
+	for(index = 0,index2 = 0;index < MAX_COINS; ++index){
+		
+		coin_x[index] = 0;
+
+		temp1 = pointer[index2]; // get a byte of data
+		coin_y[index] = temp1;
+		
+		if(temp1 == TURN_OFF) break;
+
+		++index2;
+		
+		coin_active[index] = 0;
+
+		
+		temp1 = pointer[index2]; // get a byte of data
+		coin_room[index] = temp1;
+		
+		++index2;
+		
+		temp1 = pointer[index2]; // get a byte of data
+		coin_actual_x[index] = temp1;
+		
+		++index2;
+	}
+	
+	for(++index;index < MAX_COINS; ++index){
+		coin_y[index] = TURN_OFF;
+	}
+	
+	
+	
+
+	pointer = level_1_enemies;
+	for(index = 0,index2 = 0;index < MAX_ENEMY; ++index){
+		
+		enemy_x[index] = 0;
+
+		temp1 = pointer[index2]; // get a byte of data
+		enemy_y[index] = temp1;
+		
+		if(temp1 == TURN_OFF) break;
+
+		++index2;
+		
+		enemy_active[index] = 0;
+		
+		temp1 = pointer[index2]; // get a byte of data
+		enemy_room[index] = temp1;
+		
+		++index2;
+		
+		temp1 = pointer[index2]; // get a byte of data
+		enemy_actual_x[index] = temp1;
+		
+		++index2;
+	}
+	
+	for(++index;index < MAX_ENEMY; ++index){
+		enemy_y[index] = TURN_OFF;
+	}
+}
+
+
+
+
+
+
---
+++ platformer5.h
@@ -0,0 +1,235 @@
+#define ACCEL 0x30
+#define GRAVITY 0x50
+#define MAX_SPEED 0x240
+#define JUMP_VEL -0x600
+#define MAX_RIGHT 0xb000
+
+
+
+#pragma bss-name(push, "ZEROPAGE")
+
+// GLOBAL VARIABLES
+unsigned char sprid; // remember the index into the sprite buffer
+unsigned char pad1;
+unsigned char pad1_new;
+unsigned char collision;
+unsigned char collision_L;
+unsigned char collision_R;
+unsigned char collision_U;
+unsigned char collision_D;
+unsigned char coordinates;
+unsigned char temp1;
+unsigned char temp2;
+unsigned char temp3;
+unsigned char temp4;
+unsigned int temp5;
+unsigned int temp6;
+unsigned char eject_L; // from the left
+unsigned char eject_R; // remember these from the collision sub routine
+unsigned char eject_D; // from below
+unsigned char eject_U; // from up
+unsigned char direction; //facing left or right?
+#define LEFT 0
+#define RIGHT 1
+
+int address;
+unsigned char x; // room loader code
+unsigned char y;
+unsigned char nt;
+unsigned char index;
+unsigned char index2;
+unsigned char room;
+unsigned char map;
+unsigned int scroll_x;
+unsigned int scroll_y;
+unsigned char scroll_count;
+unsigned int pseudo_scroll_x;
+//unsigned int pseudo_scroll_y;
+unsigned char L_R_switch;
+unsigned char old_x;
+//unsigned char old_y;
+unsigned char temp_x;
+unsigned char temp_y;
+unsigned char song;
+#define MAX_SONGS 2
+enum {SONG_GAME, SONG_PAUSE};
+enum {SFX_JUMP, SFX_DING, SFX_NOISE};
+
+unsigned char game_mode;
+enum {MODE_GAME, MODE_PAUSE};
+
+unsigned char coins;
+const unsigned char * pointer;
+
+
+
+
+#pragma bss-name(push, "BSS")
+
+unsigned char c_map[240];
+unsigned char c_map2[240];
+
+struct Base {
+	unsigned char x;
+	unsigned char y;
+	unsigned char width;
+	unsigned char height;
+};
+
+struct Base Generic; 
+struct Base Generic2; 
+
+struct Hero {
+	unsigned int x; // low byte is sub-pixel
+	unsigned int y;
+	signed int vel_x; // speed, signed, low byte is sub-pixel
+	signed int vel_y;
+};
+
+struct Hero BoxGuy1 = {0x4000,0xc400}; // starting position
+// the width and height should be 1 less than the dimensions (14x12)
+// note, I'm using the top left as the 0,0 on x,y
+
+#define HERO_WIDTH 13
+#define HERO_HEIGHT 11
+
+
+#define MAX_COINS 16
+unsigned char coin_x[MAX_COINS];
+unsigned char coin_y[MAX_COINS];
+unsigned char coin_active[MAX_COINS];
+unsigned char coin_room[MAX_COINS];
+unsigned char coin_actual_x[MAX_COINS];
+
+#define TURN_OFF 0xff
+#define COIN_WIDTH 7
+#define COIN_HEIGHT 11
+
+
+
+#define MAX_ENEMY 16
+unsigned char enemy_x[MAX_ENEMY];
+unsigned char enemy_y[MAX_ENEMY];
+unsigned char enemy_active[MAX_ENEMY];
+unsigned char enemy_room[MAX_ENEMY];
+unsigned char enemy_actual_x[MAX_ENEMY];
+
+#define ENEMY_WIDTH 13
+#define ENEMY_HEIGHT 13
+
+
+
+
+
+const unsigned char palette_bg[]={
+0x22, 0x16, 0x36, 0x0f,
+0, 8, 0x18, 0x39,
+0, 0, 0x10, 0x20,
+0, 0x0a, 0x1a, 0x2a
+}; 
+
+
+
+const unsigned char palette_sp[]={
+0x22, 0x01, 0x11, 0x10,
+0x22, 0x17, 0x28, 0x38,
+0x22, 0x05, 0x15, 0x35,
+0x22, 0x0f, 0x00, 0x30,
+}; 
+
+
+// 5 bytes per metatile definition, tile TL, TR, BL, BR, palette 0-3
+// T means top, B means bottom, L left,R right
+// 51 maximum # of metatiles = 255 bytes
+// 5th byte, 	1000 0000 = floor collision
+// 				0100 0000 = all collision, solid
+//				0000 0011 = palette
+
+const unsigned char metatiles1[]={
+	0, 0, 0, 0,  0,
+	2, 2, 2, 2,  3,
+	20, 20, 20, 20,  0,
+	5, 6, 21, 22,  1,
+	6, 6, 22, 22,  1,
+	6, 7, 22, 23,  1,
+	21, 22, 21, 22,  1,
+	22, 22, 22, 22,  1,
+	22, 23, 22, 23,  1,
+	8, 9, 24, 25,  1,
+	9, 9, 25, 25,  1,
+	9, 10, 25, 26,  1
+};
+
+#define COL_DOWN 0x80
+#define COL_ALL 0x40
+
+const unsigned char is_solid[]={
+	0,
+	COL_DOWN,
+	COL_ALL+COL_DOWN,
+	COL_DOWN,
+	COL_DOWN,
+	COL_DOWN,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0
+};
+
+
+
+#include "BG/Room1.c"
+#include "BG/Room2.c"
+#include "BG/Room3.c"
+#include "BG/Room4.c"
+#include "BG/Room5.c"
+
+#define MAX_ROOMS (5-1)
+#define MAX_SCROLL (MAX_ROOMS*0x100)-1
+// data is exactly 240 bytes, 16 * 15
+// doubles as the collision map data
+
+
+const unsigned char * const Rooms[]= {
+	Room1, Room2, Room3, Room4, Room5
+};
+
+//y, room, x
+//y = TURN_OFF end of list
+const unsigned char level_1_coins[]={
+	0x61,0,0x65,
+	0x81,1,0x85,
+	0x41,2,0x95,
+	0x61,3,0x75,
+	0x41,4,0x85,
+	TURN_OFF
+};
+
+//y, room, x
+//y = TURN_OFF end of list
+const unsigned char level_1_enemies[]={
+	0xc2,0,0xb0,
+	0xc2,1,0x80,
+	0xc2,2,0xc0,
+	0xc2,3,0xf0,
+	TURN_OFF
+};
+
+
+
+// PROTOTYPES
+void load_room(void);
+void draw_sprites(void);
+void movement(void);	
+void bg_collision(void);
+void draw_screen_R(void);
+void new_cmap(void);
+void bg_collision_sub(void);
+void bg_check_low(void);
+char get_position(void);
+void enemy_moves(void);
+void sprite_collisions(void);
+void check_spr_objects(void);
+void sprite_obj_init(void);
